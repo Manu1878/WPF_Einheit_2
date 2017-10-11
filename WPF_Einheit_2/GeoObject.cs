f5aae444dc36b78df6f47cd120f31c1cce193486
Manuel <wi16b035@technikum-wien.at>
2017-10-11 19:49:35 +0200

    Klasse Object hinzugefügt und bearbeitet

---
+++ WPF_Einheit_2/GeoObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Einheit_2
+{
+    class GeoObject
+    {
+
+
+        public String Shape { get; set; }
+        public int PosX { get; set; }
+        public int PosY { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        public GeoObject(string shape, int posX, int posY, int height, int width)
+        {
+            Shape = shape;
+            PosX = posX;
+            PosY = posY;
+            Height = height;
+            Width = width;
+        }
+
+    }
+}
--- WPF_Einheit_2/MainWindow.xaml.cs
+++ WPF_Einheit_2/MainWindow.xaml.cs
@@ -20,9 +20,63 @@ namespace WPF_Einheit_2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private List<GeoObject> Shapes = new List<GeoObject>();
         public MainWindow()
         {
             InitializeComponent();
+            combo.ItemsSource = new List<string>() { "Rechteck", "Kreis" };
+        }
+
+        private void Neu_Click(object sender, RoutedEventArgs e)
+        {
+            Shapes.Add(new GeoObject(
+                            combo.SelectedItem.ToString(), 
+                            int.Parse(PosX.Text), 
+                            int.Parse(PosY.Text), 
+                            int.Parse(Breite.Text), 
+                            int.Parse(Laenge.Text)));
+
+            dtgListing.ItemsSource = null;
+            dtgListing.ItemsSource = Shapes;
+        }
+
+        private void Draw_Click(object sender, RoutedEventArgs e)
+        {
+            var temp = dtgListing.SelectedItem as GeoObject;
+            Shape geoObject; // unten geo object = new rectangle und dann Canvas.Children.Add(geoObject)
+            switch (temp.Shape)
+            {
+                case "Rechteck":
+                    geoObject = new Rectangle()
+                    {
+                        Height = temp.Height,
+                        Width = temp.Width,
+                        Stroke = new SolidColorBrush(Colors.Red),
+                        StrokeThickness = 1
+                    };
+                    Canvas.Children.Add(geoObject);
+                    // Positionierung
+                    geoObject.SetValue(Canvas.TopProperty, (double)temp.PosY);
+                    geoObject.SetValue(Canvas.LeftProperty, (double)temp.PosX);
+                    break;
+
+                case "Kreis":
+                    geoObject = new Ellipse()
+                    {
+                        Height = temp.Height,
+                        Width = temp.Width,
+                        Stroke = new SolidColorBrush(Colors.Red),
+                        StrokeThickness = 1
+                    };
+                    Canvas.Children.Add(geoObject);
+                    // Positionierung
+                    geoObject.SetValue(Canvas.TopProperty, (double)temp.PosY);
+                    geoObject.SetValue(Canvas.LeftProperty, (double)temp.PosX);
+                    break;
+            }
+
+           
         }
     }
 }
